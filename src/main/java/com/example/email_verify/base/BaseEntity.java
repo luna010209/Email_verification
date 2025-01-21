d82b81d83e181375c3f105f5784e7685e7b74bbd
Lunado <liendhhha140217@gmail.com>
2025-01-21 12:02:13 +0900

    First set-up

---
+++ src/main/java/com/example/email_verify/base/BaseEntity.java
@@ -0,0 +1,25 @@
+package com.example.email_verify.base;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreatedDate
+    private LocalDateTime createdAt;
+    @LastModifiedDate
+    private LocalDateTime lastUpdateAt;
+}
